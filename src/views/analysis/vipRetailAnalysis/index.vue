3837caea6f2344ca22c11a030c42cd28f5c9dcc6
mfkdgefhn <mfkdgefhn@163.com>
2020-08-17 17:52:31 +0800

    会员购买分析提交

--- src/views/analysis/vipRetailAnalysis/index.vue
+++ src/views/analysis/vipRetailAnalysis/index.vue
@@ -20,12 +20,13 @@
         <el-col :lg="12" :md="12" :sm="12" :xs="24" class="el-col-class">
           <el-card shadow="hover" class="el-card-class">
             <pie-echarts
+              class="pie-class"
+              style="height:300px"
               :data="frequencyType"
               :tips-data="tipsData"
               :title="frequencyType.title"
               :loading="loading"
-              class="pie-class"
-              style="height:300px"
+              @readerParams="readerParams"
             />
           </el-card>
         </el-col>
@@ -33,12 +34,13 @@
         <el-col :lg="12" :md="12" :sm="12" :xs="24" class="el-col-class">
           <el-card shadow="hover" class="el-card-class">
             <pie-echarts
+              class="pie-class"
+              style="height:300px"
               :data="numberType"
               :tips-data="tipsData"
               :title="numberType.title"
               :loading="loading"
-              class="pie-class"
-              style="height:300px"
+              @readerParams="readerParams"
             />
           </el-card>
         </el-col>
@@ -48,12 +50,13 @@
         <el-col :lg="12" :md="12" :sm="12" :xs="24" class="el-col-class">
           <el-card shadow="hover" class="el-card-class">
             <pie-echarts
+              class="pie-class"
+              style="height:300px"
               :data="montyType"
               :tips-data="tipsData"
               :title="montyType.title"
               :loading="loading"
-              class="pie-class"
-              style="height:300px"
+              @readerParams="readerParams"
             />
           </el-card>
         </el-col>
@@ -61,17 +64,27 @@
         <el-col :lg="12" :md="12" :sm="12" :xs="24" class="el-col-class">
           <el-card shadow="hover" class="el-card-class">
             <pie-echarts
+              class="pie-class"
+              style="height:300px"
               :data="cycleType"
               :tips-data="tipsData"
               :title="cycleType.title"
               :loading="loading"
-              class="pie-class"
-              style="height:300px"
+              @readerParams="readerParams"
             />
           </el-card>
         </el-col>
       </el-row>
     </div>
+    <!-- 点击响应弹窗详细信息 -->
+    <prompt-box-new
+      :table-header="tableHeader"
+      :dialog-visible="dialogVisible"
+      :loading="itemLoading"
+      :title="titleNew"
+      :item-date="itemDate"
+      @handleClose="handleClose"
+    />
   </div>
 </template>
 
@@ -88,6 +101,18 @@ export default {
   },
   data() {
     return {
+      dialogVisible: false,
+      itemLoading: false,
+      titleNew: '明细',
+      itemDate: [],
+      tableHeader: [
+        { id: 1, property: 'billdate', label: '单据日期' },
+        { id: 2, property: 'cStoreName', label: '店仓' },
+        { id: 3, property: 'docno', label: '单据编号' },
+        { id: 4, property: 'mProductName', label: '款号' },
+        { id: 5, property: 'totAmtActual', label: '价格' },
+        { id: 6, property: 'attribname', label: '风格' }
+      ],
       screenHeight: window.innerHeight,
       // 加载动画
       loading: false,
@@ -149,6 +174,49 @@ export default {
     }
   },
   methods: {
+    // 渲染参数
+    readerParams(val) {
+      this.tableHeader = [
+        { property: 'billdate', label: '单据日期' },
+        { property: 'cStoreName', label: '店仓' },
+        { property: 'docno', label: '单据编号' }
+      ]
+      this.itemDate = []
+      this.dialogVisible = true
+      this.itemLoading = true
+      const paramsDate = Object.assign({}, this.params)
+      if (val.seriesName === '折扣率') {
+        this.titleNew = (paramsDate.discountRate = val.name) + ' ' + val.seriesName
+        this.tableHeader.push({ property: 'mProductName', label: '款号' })
+        this.tableHeader.push({ property: 'totAmtActual', label: '价格' })
+        this.tableHeader.push({ property: 'discountRate', label: '折扣率' })
+      } else if (val.seriesName === '单笔金额') {
+        this.titleNew = (paramsDate.singleAmount = val.name.replace(/\$/g, '')) + ' ' + val.seriesName
+        console.log(val.name.replace(/\$/g, ''))
+        this.tableHeader.push({ property: 'singleAmount', label: '单笔金额' })
+      } else if (val.seriesName === '会员积分') {
+        this.titleNew = (paramsDate.membershipPoints = val.name) + ' ' + val.seriesName
+        this.tableHeader = [
+          { property: 'cardno', label: '会员卡号' },
+          { property: 'vipname', label: '会员昵称' },
+          { property: 'mobil', label: '手机号' },
+          { property: 'birthday', label: '出生日期' },
+          { property: 'membershipPoints', label: '会员积分' }
+        ]
+      } else if (val.seriesName === '商品类别') {
+        this.titleNew = (paramsDate.commodityCategory = val.name) + ' ' + val.seriesName
+        this.tableHeader.push({ property: 'commodityCategory', label: '商品类别' })
+      }
+      this.$store.dispatch('baseApi/getRetailItemAnalysis', paramsDate).then(() => {
+        setTimeout(() => {
+          this.itemDate = this.$store.getters.retailItemAnalysis
+          this.itemLoading = false
+        }, 500)
+      }).catch(error => {
+        this.itemLoading = false
+        console.log(error)
+      })
+    },
     getAnalysis(params) {
       this.loadingCon = true
       this.loading = true
@@ -182,6 +250,9 @@ export default {
         arr.push(obj)
       })
       return arr
+    },
+    handleClose() {
+      this.dialogVisible = false
     }
 
   }
